344f28a16017866a09fdce9ea90e5908e096cf58
etowahadams <etowahadams@gmail.com>
2024-05-21 11:00:55 -0400

    feat: display ideogram track

--- src/examples/corces.ts
+++ src/examples/corces.ts
@@ -1,22 +1,34 @@
 import { PixiManager } from "../pixi-manager";
 import { GoslingTrack } from "../gosling";
 import { signal } from "@preact/signals-core";
-import { BigWigDataFetcher } from "@gosling-lang/datafetchers";
+import { BigWigDataFetcher, CsvDataFetcherClass } from "@gosling-lang/datafetchers";
 import { DataFetcher } from "@higlass/datafetchers";
 import { fakePubSub } from "../higlass/tracks/utils";
 import { gene_annotation, bigwigTracks, placTracks, ideogram } from "./corces-tracks";
 
 export function addCorces(pixiManager: PixiManager) {
+  const ideogramDomain = signal([491149952, 689445510])
+  // Ideogram track
+  const CsvDataFetcher = new CsvDataFetcherClass(ideogram.spec.data);
+  const pos0 = { x: 10, y: 10, width: 400, height: 55 };
+  new GoslingTrack(
+    ideogram,
+    ideogramDomain,
+    CsvDataFetcher,
+    pixiManager.makeContainer(pos0)
+  );
+
+  // BigWig tracks
   const xDomGenomic = signal([543317951, 544039951]);
   bigwigTracks.forEach((bigwigTrackOptions, i) => {
     const dataFetcher = new BigWigDataFetcher(bigwigTrackOptions.spec.data);
     // dataFetcher.config.cache = true; // turn on caching 
-    const pos = { x: 10, y: 10 + i * 40, width: 400, height: 40 };
+    const pos1 = { x: 10, y: pos0.y + pos0.height + i * 40, width: 400, height: 40 };
     new GoslingTrack(
       bigwigTrackOptions,
       xDomGenomic,
       dataFetcher,
-      pixiManager.makeContainer(pos)
+      pixiManager.makeContainer(pos1)
     );
   });
 
@@ -27,7 +39,7 @@ export function addCorces(pixiManager: PixiManager) {
     cacheTiles: true, // New option
   }, fakePubSub);
 
-  const pos2 = { x: 10, y: 10 + bigwigTracks.length * 40, width: 400, height: 110 };
+  const pos2 = { x: 10, y: pos0.y + pos0.height + bigwigTracks.length * 40, width: 400, height: 110 };
   new GoslingTrack(
     gene_annotation,
     xDomGenomic,
